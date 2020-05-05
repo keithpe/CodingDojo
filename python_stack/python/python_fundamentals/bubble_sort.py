368642b60f00efbbd3fe3b1fb4b7c57bb8a7d35e
Keith Peterson <keithpe@mac.com>
2020-05-05 15:42:06 -0600

    Folded while loop into bubble_sort() function. bubble_sort() function returns an integer representing number of times the array was traversed until it was sorted.

--- python_stack/python/python_fundamentals/bubble_sort.py
+++ python_stack/python/python_fundamentals/bubble_sort.py
@@ -9,41 +9,43 @@
 
 def bubble_sort(arr):
 
+    # We keep track of how many times we need to run bubble_sort() before the list is sorted.
+    counter = 0
+
+    # Use something_changed boolean variable in the while loop, to determine whether
+    # the bubble sort function has changed something.
+    # Continue to call bubble sort function on the array until it no longer makes changes.
     something_changed = False
 
-    for i in range(0, len(arr)-1, 1):
-        if arr[i] > arr[i+1]:
-            something_changed = True
-            arr[i], arr[i+1] = arr[i+1], arr[i]
+    while True:
 
-    return something_changed
+        counter += 1
+        something_changed = False
+
+        for i in range(0, len(arr)-1, 1):
+            if arr[i] > arr[i+1]:
+                something_changed = True
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+
+        # If nothing was changed this time through the array, then we're done.
+        if not something_changed:
+            break
+
+    return counter
 
 
 # Original, unsorted array
 arr = [8, 1, 5, 3, 2, 0]
 
-# We keep track of how many times we need to run bubble_sort() before the list is sorted.
-counter = 0
-
-# Use something_changed boolean variable in the while loop, to determine whether
-# the bubble sort function has changed something.
-# Continue to call bubble sort function on the array until it no longer makes changes.
-
 print("\n************** Bubble Sort **************\n")
 print("Original array is: {}\n".format(arr))
 
-while True:
-    counter += 1
-    something_changed = False
-    something_changed = bubble_sort(arr)
-    if something_changed == False:
-        break
+counter = bubble_sort(arr)
 
 print("Sorted array is: {}\n".format(arr))
 print("We called bubble_sort() {} times.\n".format(counter))
 
 print("****************** DONE *****************\n")
-
 
 '''
 SAMPLE OUTPUT BELOW
