a639b89f3433134299ff1f8c6858e5c486d2e069
Keith Peterson <keithpe@mac.com>
2020-05-05 10:29:27 -0600

    Add first two tasks in the 'update values in dictionaries and lists' section.

---
+++ python_stack/python/python_fundamentals/functions_intermediate2.py
@@ -0,0 +1,49 @@
+# Assigment:
+# Functions Intermediate II
+#
+# Objectives:
+# Practice write functions and looping over dictionaries
+# Better understanding how to traverse a list of dictionaries or through
+# a dictionary of lists.
+#
+
+# Clear the screen
+import os
+os.system('clear')
+
+x = [[5, 2, 3], [10, 8, 9]]
+students = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first-name': 'John', 'last_name': 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'curry'], 'soccer': ['Messi', 'Ronaldo', 'Roomy']
+}
+z = [{'x': 10, 'y': 20}]
+
+# ----------------------------------------------------------------------------------
+# Update values in Dictionary and Lists
+# ----------------------------------------------------------------------------------
+
+# (1) Change the value 10 in x to 15. Once you're done, x should now be [[5,2,3],[15,8,9]]
+print('\n-----------------------------------------------------------------------------------')
+print("**** Change the value 10 in x to 15. ****")
+print('-----------------------------------------------------------------------------------\n')
+print("x=", x)
+print("x[1][0]=", x[1][0])
+print("\nChange 10 in x to 15")
+x[1][0] = 15
+print("x=", x)
+
+# (2) Change the last_name of the first student from 'Jordan' to 'Bryant'
+print('\n-----------------------------------------------------------------------------------')
+print("**** Changing the last_name of the first student from 'Jordan' to 'Bryant' ****")
+print('-----------------------------------------------------------------------------------\n')
+print('students=', students)
+print('\nShow the FIRST student info')
+print('students[0]["last_name"]', students[0]['last_name'])
+print('\nCHANGE the last_name of the first student from Jordan to Bryant')
+students[0]['last_name'] = 'Bryant'
+print('\nShow all students, including Michael BRYANT')
+print('students=', students)
+print('\n-----------------------------------------------------------------------------------\n')
