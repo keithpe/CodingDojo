f54fc756ba4ee4487a36e0b1062327358e113c59
Keith Peterson <keithpe@mac.com>
2020-05-05 15:26:14 -0600

    Functioning code including bubble_sort() function. Still using outside while loop to call bubble_sort() function.

--- python_stack/python/python_fundamentals/bubble_sort.py
+++ python_stack/python/python_fundamentals/bubble_sort.py
@@ -19,17 +19,41 @@ def bubble_sort(arr):
     return something_changed
 
 
+# Original, unsorted array
 arr = [8, 1, 5, 3, 2, 0]
 
-print(arr)
+# We keep track of how many times we need to run bubble_sort() before the list is sorted.
+counter = 0
 
-# Use something_changed boolean variable to determine whether the bubble sort function
-# has changed something. Continue to call bubble sort function on the array until
-# it no longer makes changes.
+# Use something_changed boolean variable in the while loop, to determine whether
+# the bubble sort function has changed something.
+# Continue to call bubble sort function on the array until it no longer makes changes.
+
+print("\n************** Bubble Sort **************\n")
+print("Original array is: {}\n".format(arr))
+
 while True:
+    counter += 1
     something_changed = False
     something_changed = bubble_sort(arr)
     if something_changed == False:
         break
 
-print(arr)
+print("Sorted array is: {}\n".format(arr))
+print("We called bubble_sort() {} times.\n".format(counter))
+
+print("****************** DONE *****************\n")
+
+
+'''
+SAMPLE OUTPUT BELOW
+************** Bubble Sort **************
+
+Original array is: [8, 1, 5, 3, 2, 0]
+
+Sorted array is: [0, 1, 2, 3, 5, 8]
+
+We called bubble_sort() 6 times.
+
+****************** DONE *****************
+'''
