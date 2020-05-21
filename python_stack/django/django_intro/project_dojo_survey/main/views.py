3b680bd2ec96d94583d3b40a6e923315524b2ad2
Keith Peterson <keithpe@mac.com>
2020-05-20 21:14:18 -0600

    Add support for managing form submit, and view method to deal with it.

--- python_stack/django/django_intro/project_dojo_survey/main/views.py
+++ python_stack/django/django_intro/project_dojo_survey/main/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def result(request):
+    if request.method == "POST":
+        # val_from_field_one = request.POST["one"]
+        print('request.POST[]', request.POST)
+    return HttpResponse('FORM SUBMITTED')
