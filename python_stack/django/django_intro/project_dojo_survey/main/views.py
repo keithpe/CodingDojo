5559d0109fe4e29ef538252ea892c2b219c8007c
Keith Peterson <keithpe@mac.com>
2020-05-21 15:59:25 -0600

    Add radio and checkbox support. Update readme.md with my finished survey screens.

--- python_stack/django/django_intro/project_dojo_survey/main/views.py
+++ python_stack/django/django_intro/project_dojo_survey/main/views.py
@@ -7,16 +7,33 @@ def index(request):
 
 def result(request):
     if request.method == "POST":
-        # val_from_field_one = request.POST["one"]
+        print('request.POST[]', request.POST)
         name = request.POST["name"]
         location = request.POST["location"]
         language = request.POST["language"]
+        notifications = request.POST['notifications']
+
+        # checkbox has no value if not checked. Check for the existance
+        # of the dictionary key in the POST[] dictionary.
+        if 'careful_coding' in request.POST.keys():
+            careful_coding = 'yes'
+        else:
+            careful_coding = 'no'
+
+        # Checkbox issue, same as above.
+        if 'daily_blog' in request.POST.keys():
+            daily_blog = 'yes'
+        else:
+            daily_blog = 'no'
+
         comment = request.POST["comment"]
-        print('request.POST[]', request.POST)
         context = {
             'name': name,
             'location': location,
             'language': language,
+            'notifications': notifications,
+            'careful_coding': careful_coding,
+            'daily_blog': daily_blog,
             'comment': comment
         }
     return render(request, 'result.html', context)
