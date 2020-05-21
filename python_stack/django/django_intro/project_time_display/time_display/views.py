a8dae805214d5ce5aae67d3436ada9bab2ff69a0
Keith Peterson <keithpe@mac.com>
2020-05-20 19:19:45 -0600

    Use datetime.now() to retrieve date and time.

--- python_stack/django/django_intro/project_time_display/time_display/views.py
+++ python_stack/django/django_intro/project_time_display/time_display/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from time import gmtime, strftime
 
 
 def index(request):
+
+    # This is another way to retrieve the date and time
+    # print("Inside the index() method")
+    # current_time = datetime.datetime.now()
+    # print("current_time", current_time)
+    # d1 = current_time.strftime("%b %m, %Y")
+    # print("d1", d1)
+
+    # This is the way the assignment originally retrieve the date and time
+    from time import gmtime, strftime
+
     context = {
         "time": strftime("%b %m, %Y  %I:%M %p", gmtime())
     }
-    return render(request, 'index.html', context)
+
+    # This is another way to retrieve the date and time
+    from datetime import datetime
+
+    context2 = {
+        "time": datetime.now().strftime("%b %m, %Y  %I:%M %p")
+    }
+    return render(request, 'index.html', context2)
