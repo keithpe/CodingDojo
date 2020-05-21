185d9678e654e5fb467c1702a15d4f300ee58070
Keith Peterson <keithpe@mac.com>
2020-05-21 09:10:03 -0600

    Remove commented code. Clean up comments that describe different ways to get time and date info, now() and gmtime().

--- python_stack/django/django_intro/project_time_display/time_display/views.py
+++ python_stack/django/django_intro/project_time_display/time_display/views.py
@@ -3,21 +3,14 @@ from django.shortcuts import render
 
 def index(request):
 
-    # This is another way to retrieve the date and time
-    # print("Inside the index() method")
-    # current_time = datetime.datetime.now()
-    # print("current_time", current_time)
-    # d1 = current_time.strftime("%b %m, %Y")
-    # print("d1", d1)
-
-    # This is the way the assignment originally retrieve the date and time
+    # Using gmtime() is the way the assignment suggested we retrieve the date and time
     from time import gmtime, strftime
 
     context = {
         "time": strftime("%b %m, %Y  %I:%M %p", gmtime())
     }
 
-    # This is another way to retrieve the date and time
+    # Using datetime.now() is another way to retrieve the date and time
     from datetime import datetime
 
     context2 = {
