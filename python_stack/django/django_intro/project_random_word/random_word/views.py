a4c2b0af07262bca21dc7364473f46494545fb8f
Keith Peterson <keithpe@mac.com>
2020-05-22 11:46:34 -0600

    Change so that / and /random_word both point to /random_word. The assignment was originally supposed to demo having a second app in our dojo_survey project. So the / route would point to the dojo survey app.

--- python_stack/django/django_intro/project_random_word/random_word/views.py
+++ python_stack/django/django_intro/project_random_word/random_word/views.py
@@ -3,24 +3,20 @@ from django.utils.crypto import get_random_string
 
 
 def index(request):
-    if not 'attempt' in request.session:
-        return redirect('/random_word')
-
-    return render(request, 'index.html')
+    return redirect('/random_word')
 
 
 def random_word(request):
-    if request.method == 'POST':
 
-        if 'attempt' in request.session:
-            request.session['attempt'] += 1
-        else:
-            request.session['attempt'] = 1
+    if 'attempt' in request.session:
+        request.session['attempt'] += 1
+    else:
+        request.session['attempt'] = 1
 
     request.session['word'] = get_random_string(14).upper()
-    return redirect('/')
+    return render(request, 'index.html')
 
 
 def reset(request):
-    request.session['attempt'] = 1
+    request.session['attempt'] = 0
     return redirect('/random_word')
