e32e799e871daed82983f0928e18f9b98c42d574
Keith Peterson <keithpe@mac.com>
2020-05-22 08:31:21 -0600

    Correct format for day of month %d

--- python_stack/django/django_intro/project_time_display/time_display/views.py
+++ python_stack/django/django_intro/project_time_display/time_display/views.py
@@ -7,13 +7,13 @@ def index(request):
     from time import gmtime, strftime
 
     context = {
-        "time": strftime("%b %m, %Y  %I:%M %p", gmtime())
+        "time": strftime("%b %d, %Y  %I:%M %p", gmtime())
     }
 
     # Using datetime.now() is another way to retrieve the date and time
     from datetime import datetime
 
     context2 = {
-        "time": datetime.now().strftime("%b %m, %Y  %I:%M %p")
+        "time": datetime.now().strftime("%b %d, %Y  %I:%M %p")
     }
     return render(request, 'index.html', context2)
