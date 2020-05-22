1512ccd009707e5601fa57e23aa9ac060eeeaf5f
Keith Peterson <keithpe@mac.com>
2020-05-22 11:26:36 -0600

    Add support for generating random word and incrementing counter. Add support for resetting counter via route random_word/reset.

--- python_stack/django/django_intro/project_random_word/random_word/urls.py
+++ python_stack/django/django_intro/project_random_word/random_word/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('random_word', views.random_word),
+    path('random_word/reset', views.reset),
 ]
--- python_stack/django/django_intro/project_random_word/random_word/views.py
+++ python_stack/django/django_intro/project_random_word/random_word/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 
 
 def index(request):
+    if not 'attempt' in request.session:
+        return redirect('/random_word')
+
     return render(request, 'index.html')
+
+
+def random_word(request):
+    if request.method == 'POST':
+
+        if 'attempt' in request.session:
+            request.session['attempt'] += 1
+        else:
+            request.session['attempt'] = 1
+
+    request.session['word'] = get_random_string(14).upper()
+    return redirect('/')
+
+
+def reset(request):
+    request.session['attempt'] = 1
+    return redirect('/random_word')
