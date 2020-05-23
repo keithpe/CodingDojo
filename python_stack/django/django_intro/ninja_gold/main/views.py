7515fe2ac6dd386acdd4ad404fb4ba979fd6b1e7
Keith Peterson <keithpe@mac.com>
2020-05-23 10:52:02 -0600

    Change from form POST method to GET method.

--- python_stack/django/django_intro/ninja_gold/main/views.py
+++ python_stack/django/django_intro/ninja_gold/main/views.py
@@ -16,20 +16,19 @@ def index(request):
 
 def process_money(request):
     # Increment/Decrement depending on which button was pressed.
-    print("request.POST", request.POST)
-    print("request.POST['location']", request.POST['location'])
-    print("activity_log", request.session)
+    print("request.GET", request.GET)
+    print("request.GET['location']", request.GET['location'])
 
     # Determine which location button was pressed and determine
     # the amount (increased or decreased)
     gold_this_turn = 0
-    if request.POST['location'] == 'farm':
+    if request.GET['location'] == 'farm':
         gold_this_turn = random.randint(10, 20)
-    elif request.POST['location'] == 'cave':
+    elif request.GET['location'] == 'cave':
         gold_this_turn = random.randint(5, 10)
-    elif request.POST['location'] == 'house':
+    elif request.GET['location'] == 'house':
         gold_this_turn = random.randint(2, 5)
-    elif request.POST['location'] == 'casino':
+    elif request.GET['location'] == 'casino':
         gold_this_turn = random.randint(0, 50)
 
     # Determine if we're adding or removing gold from the gold stash
@@ -46,12 +45,12 @@ def process_money(request):
     # Update the total amount of gold and the activity log entry.
     if increase == True:
         request.session['gold'] += gold_this_turn
-        request.session['activity_log'] = f"<p class='green log_entry'>Earned {gold_this_turn} golds from the {request.POST['location']} ({time})\n</p>" + \
+        request.session['activity_log'] = f"<p class='green log_entry'>Earned {gold_this_turn} golds from the {request.GET['location']} ({time})\n</p>" + \
             request.session['activity_log']
         request.session['increase'] = False
     else:
         request.session['gold'] -= gold_this_turn
-        request.session['activity_log'] = f"<p class='red log_entry'>Entered a {request.POST['location']} and lost {gold_this_turn} golds... Ouch. ({time})\n</p>" + \
+        request.session['activity_log'] = f"<p class='red log_entry'>Entered a {request.GET['location']} and lost {gold_this_turn} golds... Ouch. ({time})\n</p>" + \
             request.session['activity_log']
         request.session['increase'] = False
 
