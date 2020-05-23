6c0690e02802236ea2892fabd83c134d58e1f214
Keith Peterson <keithpe@mac.com>
2020-05-22 18:29:50 -0600

    Add most HTML elements. Add CSS styling for most elements.

--- python_stack/django/django_intro/ninja_gold/main/views.py
+++ python_stack/django/django_intro/ninja_gold/main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse("Ninja Gold")
+    # return HttpResponse("Ninja Gold")
+    return render(request, 'index.html')
