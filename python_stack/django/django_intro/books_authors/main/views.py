b5c5d72f0c300b6aeab5bc088b248e0e71e3ab3a
Keith Peterson <keithpe@mac.com>
2020-06-04 09:56:31 -0600

    First draft of books and authors with templates.

--- python_stack/django/django_intro/books_authors/main/views.py
+++ python_stack/django/django_intro/books_authors/main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from main.models import *
 
-# Create your views here.
+
+def index(request):
+    print("inside index() of views.py")
+
+    # Retrieve all books in database
+    all_books = Book.objects.all()
+    print('all_books', all_books)
+
+    # Initial array to hold list of book information
+    request.session['book_list'] = ''
+
+    for book in all_books:
+        print('book.title', book.id, book.title)
+        request.session['book_list'] += f"<p id='book_id'>{book.id}</p><p id='book_title'>{book.title}</p><p id='book_action'>VIEW\n</p>"
+
+    return render(request, 'index.html')
+
+
+def add_book(request):
+    return HttpResponse("Inside add_book")
