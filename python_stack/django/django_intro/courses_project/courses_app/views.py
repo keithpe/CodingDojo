3e82901758f9e214926dd07bf0b92f65ea645437
Keith Peterson <keithpe@mac.com>
2020-06-08 10:22:38 -0600

    Add index.html and support in views.py to display it.

--- python_stack/django/django_intro/courses_project/courses_app/views.py
+++ python_stack/django/django_intro/courses_project/courses_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, redirect, HttpResponse
 
 
 def index(request):
-    return HttpResponse("This is the root route for views.py")
+    # return HttpResponse("This is the root route for views.py")
+    return render(request, "index.html")
