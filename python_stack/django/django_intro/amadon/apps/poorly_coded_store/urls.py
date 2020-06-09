5bbce97d602a180dc08dcf604047fe4d7677e483
Keith Peterson <keithpe@mac.com>
2020-06-09 15:08:06 -0600

    Add support for processing the order in a different method and redirect to the checkout method, which will in turn render the checkout html page.

--- python_stack/django/django_intro/amadon/apps/poorly_coded_store/urls.py
+++ python_stack/django/django_intro/amadon/apps/poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^checkout$', views.checkout)
+    url(r'^checkout$', views.checkout),
+    url(r'^process_order$', views.process_order)
 ]
--- python_stack/django/django_intro/amadon/apps/poorly_coded_store/views.py
+++ python_stack/django/django_intro/amadon/apps/poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+
 
 def index(request):
     context = {
@@ -7,10 +8,38 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
-def checkout(request):
+
+def process_order(request):
+
+    # Process the order here. We don't want to process
+    # in the same method that we render the checkout page.
+
+    # Calculate the order amounts here
     quantity_from_form = int(request.POST["quantity"])
     price_from_form = float(request.POST["price"])
     total_charge = quantity_from_form * price_from_form
+    print('total_charge', total_charge)
+
+    # Print to the terminal and create the order record
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+
+    # TODO: Add a try/catch for this order creation
+    Order.objects.create(
+        quantity_ordered=quantity_from_form, total_price=total_charge)
+
+    # Store current order info
+    request.session['quantity_from_form'] = quantity_from_form
+    request.session['price_from_form'] = price_from_form
+    request.session['total_charge'] = total_charge
+
+    # Keep running total of ordered and spent
+    request.session['total_ordered'] += quantity_from_form
+    request.session['total_spent'] += total_charge
+
+    # Call the checkout method, which will render the checkout page.
+    return redirect("/checkout")
+
+
+def checkout(request):
+
     return render(request, "store/checkout.html")
