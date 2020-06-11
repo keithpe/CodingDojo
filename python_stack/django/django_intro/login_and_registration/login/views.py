7a339e097118f4842c98d5a89d9a15598fbad3bf
Keith Peterson <keithpe@mac.com>
2020-06-11 16:59:23 -0600

    Add support for using bcrypt to encrypt the password. Modify the login password to use input type password, so the password appears as a serious of docs (password cannot be read on the screen).

--- python_stack/django/django_intro/login_and_registration/login/views.py
+++ python_stack/django/django_intro/login_and_registration/login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from login.models import *
 from django.contrib import messages
+import bcrypt
 
 
 def index(request):
@@ -29,9 +30,14 @@ def registration(request):
         # redirect the user back to the form to fix the errors
         return redirect('/')
 
+    # Use bcrytp to create a hash for this password and store the hashed value into our database
+    # with the rest of the user information
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
     # Create the user record for this new user,
     this_user = User(first_name=request.POST['first_name'],
-                     last_name=request.POST['last_name'], email=request.POST['email'], password=request.POST['password'], birthday=request.POST['birthday'])
+                     last_name=request.POST['last_name'], email=request.POST['email'], password=pw_hash, birthday=request.POST['birthday'])
 
     # And save it.
     this_user.save()
@@ -49,7 +55,27 @@ def login(request):
     # Process the login form
     request.session.flush()
     request.session['process'] = request.POST['process']
-    return redirect('login/success')
+
+    # See if the user email exists in the database
+    user = User.objects.filter(email=request.POST['email'])
+
+    # If user array is not empty, then get the first user (0)
+    if user:
+        logged_user = user[0]
+
+        # Use bcrypt's check_password_hash method, passing the hash from our dataase and password from the form
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+
+            # if we get True after checking the password, we may put the user id in session
+            request.session['userid'] = logged_user.id
+            request.session['first_name'] = logged_user.first_name
+
+            # Set a session variable to indicate that we've successfully logged in. (access to success page will require it.)
+            request.session['status'] = 'success'
+
+            return redirect('login/success')
+
+    return redirect('/')
 
 
 def logout(request):
