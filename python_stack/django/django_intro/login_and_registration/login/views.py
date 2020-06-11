d50d88f267be1823e1da08003521f30499a0075e
Keith Peterson <keithpe@mac.com>
2020-06-11 12:00:27 -0600

    Change to use request.session.flush(). Add in logout, and before we start to process login and registration.

--- python_stack/django/django_intro/login_and_registration/login/views.py
+++ python_stack/django/django_intro/login_and_registration/login/views.py
@@ -7,18 +7,21 @@ def index(request):
 
 def registration(request):
     # Process the registration form
+    request.session.flush()
     request.session['process'] = request.POST['process']
     return redirect('login/success')
 
 
 def login(request):
     # Process the login form
+    request.session.flush()
     request.session['process'] = request.POST['process']
     return redirect('login/success')
 
 
 def logout(request):
     # Process the logout
+    request.session.flush()
     return redirect('/')
 
 
