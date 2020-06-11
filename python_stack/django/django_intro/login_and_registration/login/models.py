30fa0e11ee0739f68a5665ceae568ba8a64f5498
Keith Peterson <keithpe@mac.com>
2020-06-11 13:06:56 -0600

    Create data class and validator. Update login form. Change password fields to type password. Update views to loop back to login form as validation fails.

--- python_stack/django/django_intro/login_and_registration/login/models.py
+++ python_stack/django/django_intro/login_and_registration/login/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+import re
 
-# Create your models here.
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+
+        errors = {}
+
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters'
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 characters'
+        if len(postData['password']) < 8:
+            errors['password_length'] = 'Password must be at least 8 characters'
+        if postData['password'] != postData['confirm_pw']:
+            errors['password_confirm'] = 'Password and confirm values are different'
+        if len(postData['email']) < 8:
+            errors['email_length'] = 'Email cannot be empty'
+
+        # Check for invalid email
+        pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        result = re.match(pattern, postData['email'])
+        if not result:
+            errors['email'] = 'Invalid email address'
+
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    birthday = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
--- python_stack/django/django_intro/login_and_registration/login/views.py
+++ python_stack/django/django_intro/login_and_registration/login/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
+from login.models import *
+from django.contrib import messages
 
 
 def index(request):
@@ -7,8 +9,39 @@ def index(request):
 
 def registration(request):
     # Process the registration form
+
+    print("inside the registration method")
+
     request.session.flush()
     request.session['process'] = request.POST['process']
+
+    print("About to run validator")
+    errors = User.objects.basic_validator(request.POST)
+    print("Finished running validator")
+
+    print("About to check for errors")
+    if errors:
+        # Loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+
+            print('error:', value)
+
+            # Save form info so we can display it back in the form when we get redirected.
+            request.session['first_name'] = request.POST['first_name']
+            request.session['last_name'] = request.POST['last_name']
+            request.session['email'] = request.POST['email']
+            request.session['password'] = request.POST['password']
+
+        # redirect the user back to the form to fix the errors
+        return redirect('/')
+
+        # Reset the request.session values
+        request.session['first_name'] = ""
+        request.session['last_name'] = ""
+        request.session['email'] = ""
+        request.session['password'] = ""
+
     return redirect('login/success')
 
 
