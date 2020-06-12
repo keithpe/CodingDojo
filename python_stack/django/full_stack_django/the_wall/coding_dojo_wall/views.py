dc2b54424fa67a5e6f4de06a1f73e83ca516b33f
Keith Peterson <keithpe@mac.com>
2020-06-12 15:35:19 -0600

    Add support for deleting messages and comments (any user can delete any message or comment. TODO: Only allow users who created messages/comments to delete them.

--- python_stack/django/full_stack_django/the_wall/coding_dojo_wall/views.py
+++ python_stack/django/full_stack_django/the_wall/coding_dojo_wall/views.py
@@ -31,6 +31,13 @@ def create_message(request):
 
 
 def delete_message(request):
+
+    # Get them message
+    this_message = Message.objects.get(id=request.POST['message_id'])
+
+    # Delete the message
+    this_message.delete()
+
     return redirect('/wall')
 
 
@@ -46,4 +53,11 @@ def create_comment(request):
 
 
 def delete_comment(request):
+
+    # Get the comment
+    this_comment = Comment.objects.get(id=request.POST['comment_id'])
+
+    # Delete the comment
+    this_comment.delete()
+
     return redirect('/wall')
