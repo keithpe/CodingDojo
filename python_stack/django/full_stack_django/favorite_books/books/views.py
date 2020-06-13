d062bedab5965b081077c1bd5873c4b124cc9a8f
Keith Peterson <keithpe@mac.com>
2020-06-13 10:24:26 -0600

    Add books app, with show_books.html and bootstrap style sheet.

--- python_stack/django/full_stack_django/favorite_books/books/views.py
+++ python_stack/django/full_stack_django/favorite_books/books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+
+
+def show_books(request):
+    # return HttpResponse('Inside show_books method')
+    return render(request, "show_books.html")
