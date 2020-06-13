165cf3feb98a1d0c41012a53bedaf6822f8d7983
Keith Peterson <keithpe@mac.com>
2020-06-13 11:17:23 -0600

    Create templates for edit and show single book. Add views.py methods and routing support in urls.py. Ready for model creation.

--- python_stack/django/full_stack_django/favorite_books/books/urls.py
+++ python_stack/django/full_stack_django/favorite_books/books/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.show_books),
+    path('<id>', views.show_book),
+    path('<id>/edit', views.edit_book),
 ]
--- python_stack/django/full_stack_django/favorite_books/books/views.py
+++ python_stack/django/full_stack_django/favorite_books/books/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render, redirect, HttpResponse
 def show_books(request):
     # return HttpResponse('Inside show_books method')
     return render(request, "show_books.html")
+
+
+def show_book(request, id):
+    return render(request, "show_book.html")
+
+
+def edit_book(request, id):
+    return render(request, "edit_book.html")
