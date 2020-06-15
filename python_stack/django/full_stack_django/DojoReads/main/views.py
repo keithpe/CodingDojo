ec46b2597010d8f930b8327c6783a98878ebe7c1
Keith Peterson <keithpe@mac.com>
2020-06-15 11:03:07 -0600

    Stubbed out all pages and routing

--- python_stack/django/full_stack_django/DojoReads/main/views.py
+++ python_stack/django/full_stack_django/DojoReads/main/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render, redirect, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    # Add new book to database
+    return redirect('/books/1')
+
+
+def show(request, id):
+    # Show current book and review
+    return render(request, 'show.html')
