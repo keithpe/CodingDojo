cde1e27d23e9f5b8bec33b5d58db649b2fdcc2c0
Keith Peterson <keithpe@mac.com>
2020-06-17 10:06:40 -0600

    modified the route for adding a review to an existing book. Modified the size of the review text.

--- python_stack/django/full_stack_django/DojoReads/main/urls.py
+++ python_stack/django/full_stack_django/DojoReads/main/urls.py
@@ -2,12 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Main Page
     path('', views.index),
+
+    # New Book (render form)
     path('new', views.new),
+
+    # Create the Book and add to database
     path('create', views.create),
-    path('create_review/<id>', views.create_review),
+
+    # Create a review (for existing book - from show book page)
+    path('<id>/review/create', views.create_review),
+
+    # Show a book
     path('<id>', views.show),
+
+    # show a user
     path('user/<id>', views.show_user),
+
+    # delete a review
     path('review/<id>/delete', views.delete_review),
 
 ]
