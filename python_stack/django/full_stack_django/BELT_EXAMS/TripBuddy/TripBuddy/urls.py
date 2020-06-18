72b0b08e2bb7602ec64071b10ccac8bd3aa4614f
Keith Peterson <keithpe@mac.com>
2020-06-18 09:47:14 -0600

    Stubbed out trips page and new trip page. with routes and views defs

--- python_stack/django/full_stack_django/BELT_EXAMS/TripBuddy/TripBuddy/urls.py
+++ python_stack/django/full_stack_django/BELT_EXAMS/TripBuddy/TripBuddy/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('login.urls')),
-    path('trips', include('trips.urls')),
+    path('trips/', include('trips.urls')),
 ]
--- python_stack/django/full_stack_django/BELT_EXAMS/TripBuddy/trips/views.py
+++ python_stack/django/full_stack_django/BELT_EXAMS/TripBuddy/trips/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render, redirect, HttpResponse
 def index(request):
     # return HttpResponse('Inside the trips index def')
     return render(request, 'index.html')
+
+
+def new(request):
+    # Display form for user to enter new trip information
+
+    return render(request, 'new.html')
