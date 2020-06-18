ccce29b52163d6e51ba095b95e9d248c2b293e29
Keith Peterson <keithpe@mac.com>
2020-06-18 10:21:26 -0600

    Stubbed out new trips form and defs for new(form) and create(process/add new trip).

--- python_stack/django/full_stack_django/BELT_EXAMS/TripBuddy/trips/views.py
+++ python_stack/django/full_stack_django/BELT_EXAMS/TripBuddy/trips/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render, redirect, HttpResponse
 
 def index(request):
     # return HttpResponse('Inside the trips index def')
+    print("Inside the INDEX def")
+
     return render(request, 'index.html')
 
 
 def new(request):
     # Display form for user to enter new trip information
+    print("Inside the NEW def")
 
     return render(request, 'new.html')
+
+
+def create(request):
+    # Create the new trip object and save it to the database
+    print("Inside the CREATE def")
+    return redirect('/trips')
