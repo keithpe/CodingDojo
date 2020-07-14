ec66c312a7c0d98811734296dcf137408b62bd42
Keith Peterson <keithpe@mac.com>
2020-07-14 16:59:42 -0600

    Add function removeEvenLengthStrings

--- algorithms/strings/todo2.js
+++ algorithms/strings/todo2.js
@@ -22,3 +22,35 @@ str = 'creature'
 console.log('original string:', str)
 str = reverseString(str)
 console.log('modified string:', str)
+
+
+//****************************************************************************
+// Remove Even-Length Strings
+//****************************************************************************
+// Build a standalone function to remove strings of even lengths from a 
+// given array. 
+// For array containing ["Nope!","Its","Kris","starting","with","K!",
+// "(instead","of","Chris","with","C)","."], 
+// change that same array to ["Nope!","Its","Chris","."].
+//****************************************************************************
+
+function removeEvenLengthStrings(arr) {
+
+    let newArr = []
+
+    for (let idx = 0; idx < arr.length; idx++) {
+        if (arr[idx].length % 2 != 0) {
+            newArr.push(arr[idx])
+        }
+
+    }
+
+    return newArr
+}
+
+
+console.log("\n**** Remove Even-Length Strings ****")
+arr = ["Nope!", "Its", "Kris", "starting", "with", "K!", "(instead", "of", "Chris", "with", "C)", "."]
+console.log('original array:', arr)
+arr = removeEvenLengthStrings(arr)
+console.log('modified array:', arr)
