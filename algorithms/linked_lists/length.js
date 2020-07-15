06bf147c0a9db35a584c0d74c5793fb2f21d7fb1
Keith Peterson <keithpe@mac.com>
2020-07-15 10:24:14 -0600

    Cleaned up instruction text at top of file.

--- algorithms/linked_lists/length.js
+++ algorithms/linked_lists/length.js
@@ -4,10 +4,10 @@
 //
 // July 20, 2013: about 5000 people wait in line
 // for a chance to audition
-// for American Idol.Create a
-//
-// function that accepts a pointer to the first list node, and returns number 
-// of nodes in that SList.
+// for American Idol.
+// 
+// Create a function that accepts a pointer to the first list node, and 
+// returns number of nodes in that SList.
 //****************************************************************************
 
 // Node Class
