f769b27c65cdc2df9c2f1ee666f3675831be2f6f
Keith Peterson <keithpe@mac.com>
2020-07-16 09:01:47 -0600

    Add function swapPairs()

--- algorithms/arrays/todo1.js
+++ algorithms/arrays/todo1.js
@@ -59,3 +59,41 @@ console.log('remove the element at index:', remove_at_index)
 console.log('remove element at array index: ' + remove_at_index);
 console.log('The value at array index ' + remove_at_index + ' was ' + removeAt(arr, remove_at_index))
 console.log('modified array: ' + arr)
+
+
+//****************************************************************************
+// Swap Pairs
+//****************************************************************************
+// Swap positions of successive pairs of values of given array. If length is 
+// odd, do not change the final element. 
+// 
+// For [1,2,3,4], return [2,1,4,3]. 
+//
+// For example, change input ["Brendan",true,42] to [true,"Brendan",42]. 
+// 
+// As with all array challenges, do this without using any built-in array 
+// methods.
+//****************************************************************************
+
+function swapPairs(arr) {
+    for (let idx = 0; idx < arr.length; idx += 2) {
+
+        // If there is no element after this one, it means we have an odd
+        // array length. Do not attempt to swap, just exit loop.
+        if (idx + 1 == arr.length) {
+            break;
+        }
+        // Save element at idx
+        let temp = arr[idx]
+        // swap idx for what is in idx+1
+        arr[idx] = arr[idx + 1]
+        // store saved temp value (from idx) to array index of idx+1 
+        arr[idx + 1] = temp
+    }
+}
+
+console.log('\n**** Swap Pairs ****')
+arr = [1, 2, 3, 4, 5, 6, 7]
+console.log('original array: ' + arr)
+swapPairs(arr)
+console.log('modified array: ' + arr)
