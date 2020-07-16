726bfb3d2c390d0a0803d0d2cf8af72df6e7a635
Keith Peterson <keithpe@mac.com>
2020-07-16 14:59:53 -0600

    Add function filterRange() to todo2.js

--- algorithms/arrays/todo2.js
+++ algorithms/arrays/todo2.js
@@ -209,3 +209,40 @@ console.log('modified array: ' + arr)
 // 
 // No built-in array functions.
 //****************************************************************************
+// TODO: The instructions say NO builtin functions AND work IN-PLACE, but 
+// I'm assuming that we can use pop as in previous examples that also allowed 
+// no builtin functions. Otherwise I would use a second array to do this.
+//****************************************************************************
+
+function filterRange(arr, min, max) {
+
+    console.log('min:', min, 'max:', max);
+
+    // Check the last element in the array. If it's not within the min/max
+    // values then pop it off the array. Repeat the same number as the
+    // array length
+
+    let original_arr_length = arr.length;
+
+    while (original_arr_length) {
+
+        if ((arr[arr.length - 1] >= min) && (arr[arr.length - 1] <= max)) {
+            temp = arr[arr.length - 1];
+            for (let idx = arr.length - 1; idx >= 0; idx--) {
+                arr[idx] = arr[idx - 1];
+            }
+            arr[0] = temp;
+        } else {
+            arr.pop();
+        }
+        original_arr_length--;
+    }
+}
+
+console.log('\n**** Filter Range ****')
+arr = [1, 7, 2, 4, 3, 5, 27];
+min = 2;
+max = 4;
+console.log('original array: ' + arr)
+filterRange(arr, min, max)
+console.log('modified array: ' + arr)
