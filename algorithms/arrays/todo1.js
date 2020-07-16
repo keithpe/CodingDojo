6182fed44c781e299469e003d77e4a28af2c8826
Keith Peterson <keithpe@mac.com>
2020-07-16 08:40:28 -0600

    Add function removeAt()

--- algorithms/arrays/todo1.js
+++ algorithms/arrays/todo1.js
@@ -25,3 +25,37 @@ arr = [1, 2, 3, 4, 5, 6, 7]
 console.log('original array: ' + arr)
 console.log(popFront(arr));
 console.log('modified array: ' + arr)
+
+
+//****************************************************************************
+// Remove At
+//****************************************************************************
+// Given an array and an index into array, remove and return the array 
+// value at that index. Do this without using built-in array methods except 
+// pop(). Think of popFront(arr) as equivalent to removeAt(arr,0).
+//****************************************************************************
+
+function removeAt(arr, index) {
+
+    // Save this element. It's the one we'll return
+    remove_element = arr[index]
+
+    // Starting with the element we want to remove..
+    // reassign the value at idx to the value to its right
+    for (let idx = index; idx < arr.length; idx++) {
+        arr[idx] = arr[idx + 1]
+    }
+    // Now the last item in the array is a dupe of the one to it's left
+    // So remove it.
+    arr.pop();
+    return remove_element;
+}
+
+console.log('\n**** Remove At ****')
+arr = [1, 2, 3, 4, 5, 6, 7]
+console.log('original array: ' + arr)
+let remove_at_index = 3
+console.log('remove the element at index:', remove_at_index)
+console.log('remove element at array index: ' + remove_at_index);
+console.log('The value at array index ' + remove_at_index + ' was ' + removeAt(arr, remove_at_index))
+console.log('modified array: ' + arr)
